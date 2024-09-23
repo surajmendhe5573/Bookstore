4f52cfd996066c29e68338b87e49bc1b06312a9b
surajmendhe5573 <surajmendhe470@gmail.com>
2024-09-23 14:04:08 +0530

    connected with postgres

--- core/settings.py
+++ core/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "Bookstore",
+        "USER": "postgres",
+        "PASSWORD": "suraj",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
